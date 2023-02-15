75768fc30ae4ca7a3dfe7265640f27fd0012dcaa
Anshum Shukla <anshumshukla0030@gmail.com>
2023-02-15 19:41:28 +0530

    ok16

--- signup.js
+++
@@ -1,7 +0,0 @@
-function checkInp() {
-    var x = document.forms["signup-form"]["phone"].value;
-    if (x.length !== 10) {
-        alert("Enter correct phone number!")
-        return false;
-    }
-}
